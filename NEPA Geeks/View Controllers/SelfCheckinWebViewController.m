5c6d0292e6f1783e89689e6b8a232532b4f65616
Matt Morgis <matt@rappiddevelopment.com>
2017-01-07 14:07:54 -0500

    feat: adding a view controller for self-checkin module

---
+++ NEPA Geeks/View Controllers/SelfCheckinWebViewController.m
@@ -0,0 +1,24 @@
+//
+//  SelfCheckinWebViewController.m
+//  NEPA Geeks
+//
+//  Created by Matt Morgis on 1/7/17.
+//  Copyright © 2017 Rappid Development LLC. All rights reserved.
+//
+
+#import "SelfCheckinWebViewController.h"
+
+@interface SelfCheckinWebViewController ()
+
+@end
+
+@implementation SelfCheckinWebViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // TODO: Move to a subclass of UINavigationController
+    self.navigationItem.title = @"NEPA Geeks";
+}
+
+@end
