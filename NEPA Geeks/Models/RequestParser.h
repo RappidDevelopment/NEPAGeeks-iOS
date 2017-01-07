920f3630026468a34755941a6919ed9bc14aaf75
Matt Morgis <matt@rappiddevelopment.com>
2017-01-07 16:43:40 -0500

    *feat: adding a request parser class

---
+++ NEPA Geeks/Models/RequestParser.h
@@ -0,0 +1,19 @@
+//
+//  RequestParser.h
+//  NEPA Geeks
+//
+//  Created by Matt Morgis on 1/7/17.
+//  Copyright © 2017 Rappid Development LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+/**
+ Parses various NSURLRequests
+ */
+@interface RequestParser : NSObject
+
+
+
+@end
---
+++ NEPA Geeks/Models/RequestParser.m
@@ -0,0 +1,15 @@
+//
+//  RequestParser.m
+//  NEPA Geeks
+//
+//  Created by Matt Morgis on 1/7/17.
+//  Copyright © 2017 Rappid Development LLC. All rights reserved.
+//
+
+#import "RequestParser.h"
+
+
+@implementation RequestParser
+
+
+@end
