7b47a769e706027dc934cbb4e059e31bbc0ca2f7
Matt Morgis <matt@rappiddevelopment.com>
2017-01-07 13:43:34 -0500

    Initial commit

---
+++ NEPA Geeks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NEPA Geeks
+//
+//  Created by Matt Morgis on 1/7/17.
+//  Copyright © 2017 Rappid Development LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
