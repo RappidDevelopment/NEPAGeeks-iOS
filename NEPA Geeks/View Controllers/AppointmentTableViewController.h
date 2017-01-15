7675d042c7477c57460b8848a2ef7449e9a386d7
Matt Morgis <matt@rappiddevelopment.com>
2017-01-14 20:48:36 -0500

    *feat: creating an appointment table view controller

---
+++ NEPA Geeks/View Controllers/AppointmentTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AppointmentTableViewController.h
+//  NEPA Geeks
+//
+//  Created by Matt Morgis on 1/14/17.
+//  Copyright © 2017 Rappid Development LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppointmentTableViewController : UIViewController
+
+@end
