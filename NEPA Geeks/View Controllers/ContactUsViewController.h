208a52d3e20192b1115cc96cf9bb2d8d09d99e6c
Matt Morgis <matt@rappiddevelopment.com>
2017-08-05 15:59:21 -0400

    feat: adding contact us view controller

---
+++ NEPA Geeks/View Controllers/ContactUsViewController.h
@@ -0,0 +1,17 @@
+//
+//  ContactUsViewController.h
+//  NEPA Geeks
+//
+//  Created by Matt Morgis on 8/5/17.
+//  Copyright © 2017 Rappid Development LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+/**
+ The view controller for the contact us page.
+ */
+@interface ContactUsViewController : UIViewController
+
+@end
